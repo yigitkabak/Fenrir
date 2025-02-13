d67e2b411a0a3dc343ffb927eaa3c0ab25c22212
Yiğit KABAK <yigitkabak@proton.me>
2025-02-14 00:26:24 +0300

    Update

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import { run } from "./runtime";
+import { Command } from "commander";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const program = new Command();
+
+program
+    .name("fenrir")
+    .description("Fenrir Runtime")
+    .version("1.0.0");
+
+program
+    .command("run <input>")
+    .description("Run a .fnr file")
+    .action((input) => {
+        const filePath = resolve(process.cwd(), input);
+        const fenrirCode = readFileSync(filePath, "utf-8");
+        run(filePath);
+    });
+
+program.parse(process.argv);
