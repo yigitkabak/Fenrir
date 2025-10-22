685b239957a55116ad1c47625314b2ba9aee97b5
Yiğit KABAK <yigitkabak@tuta.io>
2025-10-22 19:04:40 +0300

    Add files via upload

---
+++ MacApp/Fenrir/FenrirApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct FenrirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+        .windowResizability(.contentSize)
+    }
+}
